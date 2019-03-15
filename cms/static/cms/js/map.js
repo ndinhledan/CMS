c7fa54c42d95b4f280008026df2bc9378c1766b3
LeonardIsOne <32675659+LeonardisOne@users.noreply.github.com>
2019-03-15 13:41:44 +0800

    updated view-map

---
+++ cms/static/cms/js/map.js
@@ -0,0 +1,55 @@
+$(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(1.344, 103.839);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 11,
+            panControl: false,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+        var markerImage = new Image();
+        markerImage.src = 'marker.png';
+
+        addMarker({lat:1.442, lng:103.823});//north
+        addMarker({lat:1.338, lng:103.695});//west
+        addMarker({lat:1.344, lng:103.813});//central
+        addMarker({lat:1.354, lng:103.941});//east
+        addMarker({lat:1.265, lng:103.818});
+        
+        function addMarker(lat_lng){
+        var marker = new google.maps.Marker({
+          position: lat_lng,
+          map:map,
+          icon: markerImage
+        });
+        }
+
+        var contentString = '<div class="info-window">' +
+                '<h3>Info Window Content</h3>' +
+                '<div class="info-content">' +
+                '<p>Incident Type</p>' +
+                '</div>' +
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 400
+        });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+
+        var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+
+        map.set('styles', styles);
+    }
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
