1eb68ee42962f9bc083c7bbcf1aab3c99ca281b5
Matt Han <donghan37@gmail.com>
2019-03-20 13:24:18 +0800

    1). Added id as primary key 2). Added id filter

--- cms/models.py
+++ cms/models.py
@@ -27,7 +27,8 @@ class Incident(models.Model):
 		(Aftershock, 'Earthquake Aftershock'),
 		(Terrorist, 'Terrorist Activity')
 	)
-
+	id = models.AutoField(primary_key=True)
+	
 	submitter = models.ForeignKey(
 		User,
 		on_delete=models.CASCADE,
@@ -44,7 +45,7 @@ class Incident(models.Model):
 
 	incident_date = models.DateTimeField('date incident happens',
 											auto_now_add=True)
-	
+
 	assistance_type = models.ForeignKey(
 		Assistance,
 		on_delete=models.CASCADE)
@@ -69,5 +70,3 @@ class Incident(models.Model):
 
 	class Meta:
 		ordering = ["-incident_date"]
-
-
