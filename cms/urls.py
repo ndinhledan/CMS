38e19a805527af684bb633061358f45f8f948ebc
charlenelfj <charlenelfj@gmail.com>
2019-03-28 00:08:59 +0800

    front end v1

--- cms/urls.py
+++ cms/urls.py
@@ -11,5 +11,5 @@ urlpatterns =[
 	path('create-incident/', views.CreateIncidentView.as_view(),name='create-incident'),
 	path('case/<int:pk>/', views.DetailCase.as_view(), name='case'),
 	path('order/by_<condition>', views.IndexView.as_view(), name='order'),
-	path('view-map/', views.MapView.as_view(), name='view-map'),
+	path('view-map/', views.mapview, name='view-map'),
 ]
